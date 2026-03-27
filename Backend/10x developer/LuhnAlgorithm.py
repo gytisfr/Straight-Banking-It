9b8029ca8db16746b57d211409afd726879b55b8
Copciop23 <138177233+Copciop23@users.noreply.github.com>
2026-03-27 11:11:36 +0000

    Card generator

--- Backend/10x developer/LuhnAlgorithm.py
+++ Backend/10x developer/LuhnAlgorithm.py
@@ -1,24 +1,29 @@
 import random
 
-def checkCard(cardn):
-    cardchecker = []
-    for n in cardn:
-        if n == ' ':
-            continue
-        cardchecker.append(int(n))
+def checkCard(cardchecker):
+    temp = cardchecker[:]
     
-    for i in range (0, 16, 2):
-        cardchecker[i] *= 2
-        if cardchecker[i] > 9:
-            cardchecker[i] = 1 + (cardchecker[i] - 10)
+    for i in range(0, 15, 2):
+        temp[i] *= 2
+        if temp[i] > 9:
+            temp[i] = temp[i] - 9
 
-    checksum = (sum(cardchecker) - 4) % 10
-    for i in cardchecker:
-        print(cardchecker[i])
-    if checksum:
-        print("YAYYYYY")
-    else:
-        print("NOOOO")    
+    total = sum(temp)
+    checkdigit = (10 - (total % 10)) % 10
+    return checkdigit
 
-cardnumber = "4556737586899855"
-checkCard(cardnumber)
+def generateCard():
+    generatedcard = [4]
+    
+    for i in range(0, 14):
+        randomnum = random.randint(0, 9)
+        generatedcard.append(randomnum)
+
+    lastnumber = checkCard(generatedcard)
+    generatedcard.append(lastnumber)
+
+    return generatedcard
+
+card = generateCard()
+result = int(''.join(f"{i}" for i in card))
+print(result)
